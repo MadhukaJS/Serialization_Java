6ab237a458cdeb4099128d96d29f78cc74e4533f
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-22 22:44:35 +0530

    parent class and child class creted

---
+++ MySerializationDemo/src/Cat.java
@@ -0,0 +1,18 @@
+public class Cat extends Animal{
+
+    private String color;
+
+    public Cat(double speed, String color) {
+        super(speed);
+        this.color = color;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
